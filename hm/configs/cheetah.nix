90d954e5c2b198682b406a231e53e25f739b377c
Sam Lehman <github@samlehman.dev>
2024-09-27 02:08:32 -0400

    refactor: homeProfiles.base - Migrate most config from homeConfigurations.cheetah

--- hm/configs/cheetah.nix
+++ hm/configs/cheetah.nix
@@ -1,77 +1,13 @@
 { inputs, ... }:
 { config, lib, pkgs, ... }:
+#
+# NOTE: This file should only contain specifics to `homeConfigurations.cheetah`
+#
 {
   home = {
     # username = "sam";
     username = "nix-on-droid";
     stateVersion = "23.11";
-    sessionPath = [
-      "$HOME/.local/bin"
-    ];
-    sessionVariables = {
-      VISUAL = "termux-open";
-    };
-    shellAliases = rec {
-      c = "cat";
-      b = "bat --plain";
-      e = "$EDITOR";
-      ff = "ffmpeg";
-
-      # --- Dirs ---
-      l = "eza --icons";
-      ll = "${l} -l";
-      la = "${l} -a";
-      lla = "${ll} -a";
-      lal = "${ll} -a";
-      lt = "${l} --tree";
-      
-      o = "open";
-    };
-    packages = with pkgs; [
-      pls
-      starship
-      zellij
-      uutils-coreutils-noprefix
-      which
-    ];
+    packages = [];
   };
-  news.display = "show";
-  manual.manpages.enable = true;
-
-  xdg.configFile."nixpkgs/config.nix".source = (inputs.self + /nixpkgs-config.nix);
-  # nixpkgs = {
-  #   config = import (inputs.self + /nixpkgs-config.nix);
-  #   overlays = [];
-  # };
-  
-  programs.atuin.enable = true;
-  programs.bat = {
-    enable = true;
-    extraPackages = with pkgs.bat-extras; [batdiff batman batgrep batwatch];
-  };
-  programs.bottom.enable = true;
-  programs.btop.enable = true;
-  programs.command-not-found.enable = true;
-  programs.eza = {
-    enable = true;
-    git = true;
-    icons = true;
-    extraOptions = ["--group-directories-first"];
-  };
-  programs.fastfetch.enable = true;
-  programs.fd.enable = true;
-  programs.fzf.enable = true;
-  programs.gallery-dl.enable = true;
-  programs.gpg = {
-    enable = true;
-    homedir = "${config.xdg.dataHome}/gnupg";
-  };
-  programs.home-manager.enable = true;
-  programs.htop.enable = true;
-  programs.jq.enable = true;
-  programs.k9s.enable = true;
-  programs.lsd.enable = true;
-  programs.ripgrep.enable = true;
-  programs.readline.enable = true;
-  programs.navi.enable = true;
 }
--- hm/profiles/base.nix
+++ hm/profiles/base.nix
@@ -1,5 +1,100 @@
 { inputs, ... }:
 { config, lib, pkgs, ... }:
 {
-  home.homeDirectory = "/data/data/com.termux.nix/files/home";
+  home = {
+    homeDirectory = "/data/data/com.termux.nix/files/home";
+    packages = with pkgs; [
+      parallel-full             # run commands in parallel, even across machines
+      dt                        # duct tape for shell command pipelines
+      xe                        # xargs / awk alternative with pattern matching
+      uutils-coreutils-noprefix # coreutils rust rewrite
+      which                     # locate executables
+    ];
+    sessionPath = [
+      "$HOME/.local/bin"
+    ];
+    sessionVariables = {
+      # TODO: How to open in Android browser?
+      # BROWSER = "";
+      VISUAL = "termux-open";
+    };
+    shellAliases = rec {
+      b = "bat --plain";
+      c = "cat";
+      e = "$EDITOR"; v = "$VISUAL";
+      ff = "ffmpeg";
+      o = "open";
+
+      # --- Directories ---
+      l = "eza --icons";
+      ll = l + " -l";
+      la = l + " -a";
+      lla = ll + " -a";
+      lal = lla;
+      lt = l + " --tree";
+    };
+  };
+  news.display = "show";
+  manual.manpages.enable = true;
+
+  programs = {
+    atuin.enable = true;
+    bat = {
+      enable = true;
+      extraPackages = with pkgs.bat-extras; [batdiff batman batgrep batwatch prettybat];
+    };
+    bottom.enable = true;
+    btop.enable = true;
+    command-not-found.enable = true;
+    eza = {
+      enable = true;
+      git = true;
+      icons = true;
+      extraOptions = ["--group-directories-first"];
+    };
+    fastfetch.enable = true;
+    fd.enable = true;
+    fzf.enable = true;
+    gallery-dl.enable = true;
+    gpg = {
+      enable = true;
+      homedir = "${config.xdg.dataHome}/gnupg";
+    };
+    home-manager.enable = true;
+    htop.enable = true;
+    info.enable = true;
+    jq.enable = true;
+    k9s.enable = true;
+    less.enable = true;
+    lesspipe.enable = true;
+    lsd.enable = true;
+    navi.enable = true;
+    pls.enable = true;
+    rbw.enable = true;
+    readline.enable = true;
+    ripgrep.enable = true;
+    skim.enable = true;
+    starship = {
+      enable = true;
+      enableTransience = true;
+    };
+    tealdeer.enable = true;
+    thefuck.enable = true;
+    topgrade.enable = true;
+
+    # https://github.com/yt-dlp/yt-dlp#configuration
+    yt-dlp.enable = true;
+
+    zellij = {
+      enable = true;
+      enableBashIntegration = true;
+      enableZshIntegration = true;
+    };
+    zoxide.enable = true;
+  };
+  xdg.configFile."nixpkgs/config.nix".source = (inputs.self + /nixpkgs-config.nix);
+  # nixpkgs = {
+  #   config = import (inputs.self + /nixpkgs-config.nix);
+  #   overlays = [];
+  # };
 }
